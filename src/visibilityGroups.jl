5025fff8ba3f169d40fc73ae01b0b38256d25a29
Stephen Gagnon <srgagnon@buffalo.edu>
2022-02-02 19:27:50 -0500

    added visibiliy group types added functions to identify visibility groups and sun visibility groups added visibility group clustering added function to find all visibility groups via brute force

--- src/visibilityGroups.jl
+++ src/visibilityGroups.jl
@@ -2,63 +2,229 @@ module visibilityGroups
 
 using attitudeFunctions
 using LinearAlgebra
+# using Infiltrator
 
-export findVisGroup, findAllVisGroups
+import lightCurveOptimization: spaceScenario, targetObject
+import Base.==
 
-function findVisGroup(sat,scen,attitude)
+export findVisGroup, _findVisGroup, findAllVisGroups, findAllVisGroupsN, visibilityGroup, sunVisGroupClustering, sunVisGroup, visGroupClustering, findSunVisGroup
 
-    n = scen.obsNo
-    m = sat.facetNo
+const Vec{T<:Number} = AbstractArray{T,1}
+const Mat{T<:Number} = AbstractArray{T,2}
+const ArrayOfVecs{T<:Number} = Array{V,1} where V <: Vec
+const ArrayofMats{T<:Number} = Array{M,1} where M <: Mat
+const MatOrVecs = Union{Mat,ArrayOfVecs}
+const MatOrVec = Union{Mat,Vec}
+const anyAttitude = Union{Mat,Vec,DCM,MRP,GRP,quaternion}
+
+struct visibilityGroup
+    isVisible :: Array{Bool,2}
+    isConstraint :: Array{Bool,2}
+end
+
+struct sunVisGroup
+    isVisible :: Array{Bool,1}
+end
+
+function ==(x1 :: sunVisGroup, x2 :: sunVisGroup)
+    return all(x1.isVisible .== x2.isVisible)
+end
+
+function ==(x1 :: visibilityGroup, x2 :: visibilityGroup)
+    return all(x1.isVisible .== x2.isVisible)
+end
+
+function findVisGroup(sat :: targetObject, scen :: spaceScenario, attitude :: anyAttitude)
 
     (sunVec,obsVecs) = toBodyFrame(attitude,scen.sunVec,scen.obsVecs)
 
-    visGroup = Array{Bool,2}(undef,n+1,m)
+    visGroup = _findVisGroup(sat.nvecs,sunVec,obsVecs,sat.facetNo,scen.obsNo)
 
-    for i = 1:n+1
-        for j = 1:m
-            if size(obsVecs) == (3,n)
-                if i == n+1
-                    if dot(sunVec,view(sat.nvecs,:,j)) > 0
-                        visGroup[i,j] = true
-                    else
-                        visGroup[i,j] = false
-                    end
-                elseif dot(view(obsVecs,:,i),view(sat.nvecs,:,j)) > 0
-                    visGroup[i,j] = true
-                else
-                    visGroup[i,j] = false
-                end
-            elseif size(obsVecs) == (n,)
-                if i == n+1
-                    if dot(sunVec,sat.nvecs[j]) > 0
-                        visGroup[i,j] = true
-                    else
-                        visGroup[i,j] = false
-                    end
-                elseif dot(obsVecs[i],sat.nvecs[j]) > 0
-                    visGroup[i,j] = true
-                else
-                    visGroup[i,j] = false
-                end
-            end
-        end
-    end
     return visGroup
 end
 
-function findAllVisGroups(sat,scen)
+function findSunVisGroup(sat :: targetObject, scen :: spaceScenario, attitude :: anyAttitude)
 
-    n = scen.obsNo
-    m = sat.facetNo
+    if typeof(attitude) == Array{Float64,1}
+        if length(attitude) == 4
+            sunVec = qRotate(attitude,scen.sunVec)
+        end
+    else
+        error()
+    end
 
-    vgnmax = 2^(n+m)
+    visGroup = _findSunVisGroup(sat.nvecs,sunVec,sat.facetNo)
 
-
-
-
-
+    return visGroup
 end
 
+function _findVisGroup(nvecs :: Mat,sunVec :: Vec,obsVecs :: Mat,facetNo :: Int64,obsNo :: Int64)
 
+    visGroup = Array{Bool,2}(undef,obsNo+1,facetNo)
+    isConstraint = Array{Bool,2}(undef,obsNo+1,facetNo)
+    #sunVis = Array{Bool,1}(undef,facetNo)
+
+    for i = 1:obsNo+1
+        for j = 1:facetNo
+            if i == 1
+                if dot(sunVec,nvecs[j]) > 0
+                    visGroup[1,j] = true
+                    isConstraint[:,j] .= true
+                else
+                    visGroup[1,j] = false
+                    isConstraint[1,j] = true
+                    isConstraint[2:end,j] .= false
+                    #sunVis[j] = false
+                end
+
+            elseif (dot(view(obsVecs,:,i-1),view(nvecs,:,j)) > 0) #& (sunVis[j])
+                visGroup[i,j] = true
+            else
+                visGroup[i,j] = false
+            end
+        end
+    end
+    #constrNo = facetNo + length(findall(visGroup[2:end,:]))
+
+    return visibilityGroup(visGroup,isConstraint)
+end
+
+function _findVisGroup(nvecs :: ArrayOfVecs, sunVec :: Vec,obsVecs :: ArrayOfVecs, facetNo :: Int64, obsNo:: Int64)
+
+    visGroup = Array{Bool,2}(undef,obsNo+1,facetNo)
+    isConstraint = Array{Bool,2}(undef,obsNo+1,facetNo)
+    #sunVis = Array{Bool,1}(undef,facetNo)
+
+    for i = 1:obsNo+1
+        for j = 1:facetNo
+            if i == 1
+                if dot(sunVec,nvecs[j]) > 0
+                    visGroup[1,j] = true
+                    isConstraint[:,j] .= true
+                    #sunVis[j] = true
+                else
+                    visGroup[1,j] = false
+                    isConstraint[1,j] = true
+                    isConstraint[2:end,j] .= false
+                    #sunVis[j] = false
+                end
+            elseif (dot(obsVecs[i-1],nvecs[j]) > 0)# & (sunVis[j])
+                visGroup[i,j] = true
+            else
+                visGroup[i,j] = false
+            end
+        end
+    end
+    return visibilityGroup(visGroup,isConstraint)
+end
+
+function _findSunVisGroup(nvecs :: ArrayOfVecs,sunVec :: Vec, facetNo :: Int64)
+
+    visGroup = Array{Bool,1}(undef,facetNo)
+    #sunVis = Array{Bool,1}(undef,facetNo)
+
+    for j = 1:facetNo
+        if dot(sunVec,nvecs[j]) > 0
+            visGroup[j] = true
+        else
+            visGroup[j] = false
+        end
+    end
+
+    return sunVisGroup(visGroup)
+end
+
+# function findAllVisGroups(sat :: targetObject, scen :: spaceScenario)
+#
+#     n = scen.obsNo
+#     m = sat.facetNo
+#
+#     vgnmax = 2^(n+m)
+# end
+
+function findAllVisGroupsN(nvecs :: ArrayOfVecs, sunVec :: Vec,obsVecs :: ArrayOfVecs, facetNo :: Int64, obsNo:: Int64, varin, attType = :Random)
+
+    if attType == :Random
+        x = randomAtt(in,quaternion)
+        N = varin
+    elseif attType == :Specified
+        N = size(varin,2)
+        x = Vector{Vector{Float64}}(undef,N)
+        for i = 1:N
+            x[i] = varin[:,i]
+        end
+    end
+
+    visGroup = findVisGroup(sat,scen,x[1])
+    group = (visGroup.isVisible .& visGroup.isConstraint)
+
+    uniqueVisGroups = Array{typeof(group),1}(undef,1)
+    groupNo = [1]
+    groupAtt = Array{Array{Array{Float64,1},1},1}(undef,1)
+    groupAtt[1] = [x[1]]
+
+    uniqueVisGroups[1] = group
+
+    for i = 2:N
+        visGroup = findVisGroup(sat,scen,x[i])
+        group = (visGroup.isVisible .& visGroup.isConstraint)
+        # if size(group)!=size(uniqueVisGroups[1])
+        #     @infiltrate
+        # end
+        if length(findall([group] .== uniqueVisGroups)) == 0
+            append!(uniqueVisGroups,[group])
+            append!(groupNo,1)
+            append!(groupAtt,[[x[i]]])
+        else
+            # if length(findall([group] .== uniqueVisGroups)) == 0
+            #     @infiltrate
+            # end
+            groupNo[findall([group] .== uniqueVisGroups)[1]] += 1
+            append!(groupAtt[findall([group] .== uniqueVisGroups)[1]],[x[i]])
+        end
+
+    end
+    return uniqueVisGroups, groupNo, groupAtt
+end
+
+function sunVisGroupClustering(x :: ArrayOfVecs, N :: Int64, ind, visGroups, sat, scen)
+
+    # ind = Array{Int64,1}(undef,)
+    for i = 1:length(x)
+
+        group = findSunVisGroup(sat,scen,x[i])
+
+        if isempty(visGroups)
+            append!(visGroups,[group])
+            ind[i] = length(visGroups)
+        elseif any([group] .== visGroups)
+            ind[i] = findall([group] .== visGroups)[1]
+        else
+            # @infiltrate
+            append!(visGroups,[group])
+            ind[i] = length(visGroups)
+        end
+    end
+end
+
+function visGroupClustering(x :: ArrayOfVecs, N :: Int64, ind :: Vector{Int64}, visGroups :: Vector{visibilityGroup}, sat, scen)
+
+    # ind = Array{Int64,1}(undef,)
+    for i = 1:length(x)
+
+        group = findVisGroup(sat,scen,x[i]) :: visibilityGroup
+
+        if isempty(visGroups)
+            append!(visGroups,[group])
+            ind[i] = length(visGroups)
+        elseif any([group] .== visGroups)
+            ind[i] = findall([group] .== visGroups)[1]
+        else
+            # @infiltrate
+            append!(visGroups,[group])
+            ind[i] = length(visGroups)
+        end
+    end
+end
 
 end # module
